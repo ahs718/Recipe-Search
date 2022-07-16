f614caaf96ecff98b903cf7bf6d9fca9274c8c57
Aiden Schulman <aidenhschulman@gmail.com>
2022-07-16 01:06:42 -0400

    Added overall app code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Recipe from "./Recipe";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  const APP_ID = "8f8b763a";
+  const APP_KEY = "3a6016099ad73dbd54eda2b5b713ca56";
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  // Asynchronously fetches api data based off of search query
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
+
+  // Update search bar while user types
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  // Gets search after form is submitted and sets it to query
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+  
+  // Invokes the function to fetch the API data whenever the state of query is updated
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+
+  // Implement the recipe component into html
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+        <button className="search-btn" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe) => (
+          <Recipe
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
